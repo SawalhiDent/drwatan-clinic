d814adb2ef8ab4bd90f13d44a2b9722d050af8a8
SawalhiDent <sawalhimw@gmail.com>
2026-02-20 05:27:59 +0000

    db fix

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,9 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
 import * as schema from "@shared/schema";
 import path from "path";
 
 const dbPath = path.resolve("database.db");
-const sqlite = new Database(dbPath);
+const client = createClient({ url: file:${dbPath} });
 
-sqlite.pragma("journal_mode = WAL");
-sqlite.pragma("foreign_keys = ON");
-
-export const db = drizzle(sqlite, { schema });
+export const db = drizzle(client, { schema });
